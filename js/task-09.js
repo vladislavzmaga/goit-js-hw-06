78edc713cf6fbbe4d50d4a335b50c83b03150e2b
Vladislav Zmaga <vladislavzmaga@gmail.com>
2022-08-08 18:33:36 +0300

    hw1

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,13 @@
-const buttonEl = document.querySelector(".change-color");
-const parEl = document.querySelector(".color");
-
-buttonEl.addEventListener("click", getColodBody);
-
-function getColodBody() {
-  const backgroundColor = getRandomHexColor();
-  document.body.style.backgroundColor = backgroundColor;
-  parEl.textContent = getRandomHexColor();
-}
+const color = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColor = () => {
+  const backgroundColor = getRandomHexColor();
+  document.body.style.backgroundColor = backgroundColor;
+  color.textContent = backgroundColor;
+};
+
+changeColorBtn.addEventListener("click", changeColor);
